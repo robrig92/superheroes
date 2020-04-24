c02787dd8af54b2d55a477a3b188d0db1d948cdb
Roberto Padilla <robert.ipo@hotmail.com>
2020-04-23 22:53:36 -0500

    added cors

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const router = require('./routes/index');
 const Sequelize = require('sequelize').Sequelize;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const config = require('./config/config.json');
 
 let app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors);
 app.use(bodyParser.urlencoded({
     extended: false
 }));
