54d4bfde4f043c43d5dcbf7643022fc4840ffc5a
Roberto Padilla <rob@pop-os.localdomain>
2020-05-31 21:01:09 -0500

    removed users store endpoint from jwt validation middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,11 +4,10 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 const validateJwt = require('../middlewares/jwt').validateJwt;
 
-router.use('/users', validateJwt);
-router.get('/users', usersController.index);
+router.get('/users', validateJwt, usersController.index);
 router.post('/users', usersController.store);
-router.get('/users/:id', usersController.show);
-router.put('/users/:id', usersController.update);
-router.delete('/users/:id', usersController.destroy);
+router.get('/users/:id', validateJwt, usersController.show);
+router.put('/users/:id', validateJwt, usersController.update);
+router.delete('/users/:id', validateJwt, usersController.destroy);
 
 module.exports = router;
