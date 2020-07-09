28dba3aeacc3fd89122368b6b987a686bd71a524
Roberto Padilla <robert.ipo@hotmail.com>
2020-07-09 12:26:12 -0500

    fixed array validation bug

--- server/validations/schemas/heroes.js
+++ server/validations/schemas/heroes.js
@@ -5,7 +5,7 @@ const Joi = require('@hapi/joi');
 const Heroe = Joi.object({
     name: Joi.string().required(),
     age: Joi.number().integer().required(),
-    powers: Joi.array().items(Joi.number().integer()).required()
+    powers: Joi.any().required()
 });
 
 module.exports = Heroe;
