e487cd72d2b46c1fb0346e787a7771954c2b1d0d
Roberto Padilla <robert.ipo@hotmail.com>
2020-07-13 22:31:17 -0500

    Removed index heroes from JWT protection

--- server/routes/heroes.js
+++ server/routes/heroes.js
@@ -7,18 +7,17 @@ const validateJwt = require('../middlewares/jwt').validateJwt;
 const validateHeroe = require('../validations/heroes');
 const validateScore = require('../validations/heroe-scores');
 
-router.use('/heroes', validateJwt);
 router.get('/heroes', heroesController.index);
-router.post('/heroes', validateHeroe, heroesController.store);
-router.get('/heroes/:id', heroesController.show);
-router.put('/heroes/:id', validateHeroe, heroesController.update);
-router.delete('/heroes/:id', heroesController.destroy);
+router.post('/heroes', validateJwt, validateHeroe, heroesController.store);
+router.get('/heroes/:id', validateJwt, heroesController.show);
+router.put('/heroes/:id', validateJwt, validateHeroe, heroesController.update);
+router.delete('/heroes/:id', validateJwt, heroesController.destroy);
 
 // Scores
-router.get('/heroes/:id/scores', scoresController.index);
-router.post('/heroes/:id/scores', validateScore, scoresController.store);
-router.get('/heroes/:id/scores/:score_id', scoresController.show);
-router.put('/heroes/:id/scores/:score_id', validateScore, scoresController.update);
-router.delete('/heroes/:id/scores/:score_id', scoresController.destroy);
+router.get('/heroes/:id/scores', validateJwt, scoresController.index);
+router.post('/heroes/:id/scores', validateJwt, validateScore, scoresController.store);
+router.get('/heroes/:id/scores/:score_id', validateJwt, scoresController.show);
+router.put('/heroes/:id/scores/:score_id', validateJwt, validateScore, scoresController.update);
+router.delete('/heroes/:id/scores/:score_id', validateJwt, scoresController.destroy);
 
 module.exports = router;
