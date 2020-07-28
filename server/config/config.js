9e31aed6f99ce7d8b159188648078d057b2737cd
Roberto Padilla <robert.ipo@hotmail.com>
2020-07-27 22:13:06 -0500

    changed config

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ module.exports = {
         "password": process.env.DATABASE_PASSWORD,
         "database": process.env.DATABASE,
         "host": process.env.DATABASE_HOST,
-        "dialect": "mysql",
+        "dialect": "postgres",
         "operatorsAliases": false
     },
     "production": {
@@ -22,7 +22,7 @@ module.exports = {
         "password": process.env.DATABASE_PASSWORD,
         "database": process.env.DATABASE,
         "host": process.env.DATABASE_HOST,
-        "dialect": "mysql",
+        "dialect": "postgres",
         "operatorsAliases": false
     }
 }
