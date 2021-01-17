ae03193650f69af3cf9781f859c1f76042b0546b
Roberto Padilla <ripori20@gmail.com>
2021-01-17 15:21:16 -0600

    Spaces refactor

--- server/services/powers.js
+++ server/services/powers.js
@@ -6,16 +6,20 @@ module.exports = {
     async findAll() {
         return await powersRepository.findAll();
     },
+
     async store(power) {
         return await powersRepository.store(power);
     },
+
     async retrieve(id) {
         return await powersRepository.retrieve(id);
     },
+
     async update(power, args) {
         power.name = args.name;
         return await powersRepository.update(power);
     },
+
     async destroy(power) {
         return await powersRepository.destroy(power);
     }
